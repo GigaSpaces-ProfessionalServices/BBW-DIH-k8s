e70cb73490615fdda5832355d96c31288bad51ab
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-02-12 17:10:28 +0200

    updated umbrella installation to 16.3 and ingress

--- get-ui-url.sh
+++ get-ui-url.sh
@@ -2,12 +2,24 @@
 clustername=$(kubectl config current-context)
 clusteringress=$(kubectl get svc ingress-nginx-controller |awk '{print $4}'|tail -1)
 
+ 
 echo "============================================="
 echo "GUI url list for cluster [ $clustername ]"
 echo "--------------------------------------------"
+
+if [[ $clusteringress = "pending" ]];then
+    echo "Waiting for Public IP ..."
+        while [[ $clusteringress = "pending" ]]; do
+            printf '.' > /dev/tty
+            sleep 1
+        done;
+fi
+echo
 echo "ops-manager:     http://${clusteringress}:8090"
 echo 
-echo "grafana:         http://${clusteringress}:3000"
+echo "SpaceDeck        http://${clusteringress}:3000"
+echo
+echo "grafana:         http://${clusteringress}:3030"
 echo
 echo "k8s dashboard:   https:/${clusteringress}"
 echo "============================================="
--- install-dih-umbrella.sh
+++ install-dih-umbrella.sh
@@ -2,16 +2,19 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
+
 echo 
 echo "Installing DIH umbrella on k8s cluster"
 
 ### Add helm repositories
 helm repo add gigaspaces https://resources.gigaspaces.com/helm-charts
+helm repo add gigaspaces-ea https://resources.gigaspaces.com/helm-charts-ea
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 ### Install the gs-dih umbrella
-helm install xap gigaspaces/xap --version 16.2.1 -f helm/gigaspaces.yaml
+helm install xap gigaspaces-ea/xap --version 16.3.0-m6 -f helm/gigaspaces.yaml --set spacedeck.image.tag=1.0.21
+#kubectl config set-context --current --namespace=$namespace
 
 ### Install k8s dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
