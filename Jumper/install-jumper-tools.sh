231247a86d860ef11655e979b2ef8fd6554a3c19
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-02-14 00:17:40 +0200

    jumper tools updated

--- Jumper/install-jumper-tools.sh
+++ Jumper/install-jumper-tools.sh
@@ -17,5 +17,5 @@ sudo yum install -y wget vim unzip git terraform azure-cli maven
 wget https://s3.eu-west-1.amazonaws.com/shmulik.kaufman/bbw/jdk-11.0.17_linux-x64_bin.rpm
 sudo rpm -ivh jdk-11.0.17_linux-x64_bin.rpm
 rm -rf ./get_helm.sh jdk-11.0.17_linux-x64_bin.rpm
-echo "clear" >>  ~/home/centos/.bashrc
+echo "clear" >> /home/centos/.bashrc
 echo "cat ~/BBW-DIH-k8s/Jumper/.banner.txt" >> /home/centos/.bashrc
