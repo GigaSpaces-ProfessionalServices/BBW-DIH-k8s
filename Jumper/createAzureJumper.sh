2da40373807e9d948865d0ec6647434bcd4c2627
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-04-18 23:09:56 +0300

    Jumper scripts updated

--- Jumper/createAzureJumper.sh
+++ Jumper/createAzureJumper.sh
@@ -6,7 +6,7 @@ read -p "Tags | Project: " project
 
 default_tags="gspolicy=noprod"
 image="OpenLogic:CentOS:7_9:latest"
-userdata="~/azure-jumper-userdata.txt"
+userdata="./azure-jumper-userdata.txt"
 
 echo "Starting Jumper VM provisioning..."
 az vm create -n bbw-jumper-$vmname -g csm-bbw --image $image --custom-data $userdata --ssh-key-value $sshpubkey --public-ip-sku Standard --nic-delete-option delete --admin-username centos --tags Owner=$owner Project=$project $default_tags --vnet-name csm-bbw-vnet --subnet jumper
