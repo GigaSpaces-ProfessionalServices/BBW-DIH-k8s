f17b26e3cf2665a94854457301c4d170537544ec
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-04-18 23:41:02 +0300

    terraform|variables.tmp|sku updated to Standard

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,17 +3,17 @@ variable "aks_cluster" {
   default = {
     resource_group_name              = "csm-bbw"
     resource_group_location          = "eastus"
-    cluster_name                     = "Shmulik"
-    dns_prefix                       = "Shmulik"
+    cluster_name                     = "sandbox"
+    dns_prefix                       = "sandbox"
     node_pool_name                   = "bbwnodepool"
     vm_size                          = "Standard_B4ms"        # Standard_B4ms: 4vCPU, 16GB   Standard_B8ms: 8vCPU, 32GB
-    sku_tier                         = "Paid"
+    sku_tier                         = "Standard"
     enable_auto_scaling              = "true"
     agent_count                      = 1
     min_count                        = 3
     max_count                        = 5
     http_application_routing_enabled = true
-    log_analytcs_name                = "Shmulik"
+    log_analytcs_name                = "sandbox"
     log_analytcs_retention_in_days   = 30
     log_analytcs_sku                 = "PerGB2018"
 
